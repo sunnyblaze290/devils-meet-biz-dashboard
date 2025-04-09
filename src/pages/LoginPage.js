93cd85e456d0aa48573ca5a1714ca0ea7c12bab3
sunnyblaze290 <61949601+sunnyblaze290@users.noreply.github.com>
2025-04-09 03:16:24 -0700

    Create LoginPage.js

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function LoginPage() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-2xl font-bold">Business Login</h1>
+      <p className="mt-4">Login form will go here.</p>
+    </div>
+  );
+}
