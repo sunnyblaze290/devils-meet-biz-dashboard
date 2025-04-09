30e8dd88149501aee9f8113c41c1aaeacea75be9
sunnyblaze290 <61949601+sunnyblaze290@users.noreply.github.com>
2025-04-09 03:16:49 -0700

    Create DashboardPage.js

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DashboardPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Create / Manage Ads</h1>
+      <p className="mt-4">Form to create ads goes here.</p>
+    </div>
+  );
+}
